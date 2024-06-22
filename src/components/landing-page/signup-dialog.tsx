be9a1fd3af1dcc8aec703c2dc5f07ff550dcb863
Maze Winther <innoworldgames@gmail.com>
2024-06-22 20:46:42 +0200

    Added email/signup logic

--- src/components/landing-page/signup-dialog.tsx
+++ src/components/landing-page/signup-dialog.tsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogFooter,
-  DialogDescription,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { GoogleIcon } from "@/components/icons";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "@/firebase/config";
 import toast from "react-hot-toast";
 
@@ -20,6 +24,26 @@ export default function SignupDialog({
   isOpen: boolean;
   onClose: () => void;
 }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignupWithEmail = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (result.user) {
+        onClose();
+        toast.success("Wohoo! You can now start transcribing.");
+      }
+    } catch (error) {
+      toast.error("Failed to sign up. Please try again.");
+    }
+  };
+
   const handleSignupWithGoogle = async () => {
     const result = await signInWithPopup(auth, new GoogleAuthProvider());
     if (result.user) {
@@ -34,30 +58,41 @@ export default function SignupDialog({
         <DialogHeader>
           <DialogTitle className="text-center text-2xl">Sign Up</DialogTitle>
         </DialogHeader>
-        <form>
+        <form onSubmit={handleSignupWithEmail}>
           <div className="mb-4">
-            <Input type="text" placeholder="Username" required />
+            <Input
+              type="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="mb-4">
-            <Input type="email" placeholder="Email" required />
-          </div>
-          <div className="mb-4">
-            <Input type="password" placeholder="Password" required />
+            <Input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <DialogFooter>
             <Button className="w-full" type="submit">
-              Sign Up
+              Sign Up with Email
             </Button>
           </DialogFooter>
         </form>
-        <Button
-          variant="outline"
-          className="space-x-2"
-          onClick={handleSignupWithGoogle}
-        >
-          <GoogleIcon />
-          <span>Sign Up with Google</span>
-        </Button>
+        <div>
+          <Button
+            variant="outline"
+            className="w-full space-x-2"
+            onClick={handleSignupWithGoogle}
+          >
+            <GoogleIcon />
+            <span>Sign Up with Google</span>
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
