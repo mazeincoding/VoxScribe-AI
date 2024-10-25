13c305fc2c84107f77cc8399ee0a31e809593884
Maze Winther <innoworldgames@gmail.com>
2024-10-25 22:01:30 +0200

    Login / signup card vertical center only when enough height

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,9 @@
 import { AuthForm } from "@/components/auth-form";
 
-export default function SignupPage() {
+export default function LoginPage() {
   return (
     <section className="mx-auto flex min-h-dvh flex-col items-center justify-center px-6 py-8 lg:py-0">
-      <AuthForm type="signup" />
+      <AuthForm type="login" />
     </section>
   );
 }
